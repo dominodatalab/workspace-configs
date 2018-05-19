39d15955f2bd76cf8264848aca0b142feeebfe7d
Maksim Kouznetsov <maksim.kouznetsov@dominodatalab.com>
2018-05-19 09:53:50 -0700

    Add base_url back to test.

--- Jupyterlab/start.sh
+++ Jupyterlab/start.sh
@@ -12,6 +12,7 @@ cat >> $CONF_FILE << EOF
 c = get_config()
 c.LabApp.notebook_dir = '${DRT_WORKING_DIR:-"/"}'
 c.LabApp.tornado_settings = {'headers': {'Content-Security-Policy': 'frame-ancestors *'}, 'static_url_prefix': '${PREFIX}static/'}
+c.LabApp.base_url = '${PREFIX}'
 c.LabApp.default_url = '/lab/tree${DOMINO_WORKING_DIR}'
 c.LabApp.token = u''
 EOF
