3d53b45d27371a24af7ecf266205440974429429
ChuckRIHead <chuckhead@gmail.com>
2018-05-22 22:01:49 -0700

    removing base_kernel_url and base_project_url from Jupyterlab start script

--- Jupyterlab/start.sh
+++ Jupyterlab/start.sh
@@ -11,8 +11,6 @@ cat >> $CONF_FILE << EOF
 c = get_config()
 c.NotebookApp.notebook_dir = '${DRT_WORKING_DIR:-"/"}'
 c.NotebookApp.base_url = '${PREFIX}'
-c.NotebookApp.base_kernel_url = '${PREFIX}'
-c.NotebookApp.base_project_url = '${PREFIX}'
 c.NotebookApp.tornado_settings = {'headers': {'Content-Security-Policy': 'frame-ancestors *'}, 'static_url_prefix': '${PREFIX}static/'}
 c.NotebookApp.default_url = '/lab/tree${DOMINO_WORKING_DIR}'
 c.NotebookApp.token = u''
