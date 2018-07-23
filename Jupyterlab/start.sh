cc5c4b6aa89cea5606c7ad2d098b6dbd1aa83689
ChuckRIHead <chuckhead@gmail.com>
2018-07-22 17:08:22 -0700

    updating Jupyters

--- Jupyterlab/start.sh
+++ Jupyterlab/start.sh
@@ -1,15 +1,14 @@
-#!/bin/bash
 set -o nounset -o errexit
 
-CONF_DIR="$HOME/.ipython/profile_default"
+CONF_DIR="$HOME/.jupyter"
+CONF_FILE="${CONF_DIR}/jupyter_notebook_config.py"
 mkdir -p "${CONF_DIR}"
 
-CONF_FILE="${CONF_DIR}/ipython_notebook_config.py"
 PREFIX=/${DOMINO_PROJECT_OWNER}/${DOMINO_PROJECT_NAME}/notebookSession/${DOMINO_RUN_ID}/
 
 cat >> $CONF_FILE << EOF
 c = get_config()
-c.NotebookApp.notebook_dir = '${DRT_WORKING_DIR:-"/"}'
+c.NotebookApp.notebook_dir = '/'
 c.NotebookApp.base_url = '${PREFIX}'
 c.NotebookApp.tornado_settings = {'headers': {'Content-Security-Policy': 'frame-ancestors *'}, 'static_url_prefix': '${PREFIX}static/'}
 c.NotebookApp.default_url = '/lab/tree${DOMINO_WORKING_DIR}'
