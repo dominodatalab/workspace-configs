c5c2707beb410f87dc4cc8f5e5e2f6a229114230
Sujay Mahajan <sujay.mahajan@dominodatalab.com>
2019-03-25 18:00:00 -0700

    Update start.sh
    adding startup script that works from VIP project

--- superset/start.sh
+++ superset/start.sh
@@ -4,23 +4,20 @@ export SUP_ROW_LIMIT=5000
 export SUP_SECRET_KEY='thisismysecretkey' 
 export SUP_CSRF_ENABLED=True
 
-## Add gunicorn arguments
 export GUNICORN_BIND=0.0.0.0:8088
 export GUNICORN_LIMIT_REQUEST_FIELD_SIZE=0
 export GUNICORN_LIMIT_REQUEST_LINE=0
 export GUNICORN_TIMEOUT=60
 export GUNICORN_WORKERS=2
-export GUNICORN_CMD_ARGS="--workers ${GUNICORN_WORKERS} --timeout ${GUNICORN_TIMEOUT} --bind ${GUNICORN_BIND} --limit-request-line ${GUNICORN_LIMIT_REQUEST_LINE} --limit-request-field_size ${GUNICORN_LIMIT_REQUEST_FIELD_SIZE}" 
- 
- 
+export GUNICORN_CMD_ARGS="--workers ${GUNICORN_WORKERS} --timeout ${GUNICORN_TIMEOUT} --bind ${GUNICORN_BIND} --limit-request-line ${GUNICORN_LIMIT_REQUEST_LINE} --limit-request-field_size ${GUNICORN_LIMIT_REQUEST_FIELD_SIZE}"
+
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
  
- 
 export SUPERSET_HOME=$DOMINO_WORKING_DIR
 export SUP_META_DB_URI=sqlite:///$DOMINO_WORKING_DIR/superset.db
-export PYTHONPATH=$DOMINO_WORKING_DIR:$PYTHONPATH
- 
+export PYTHONPATH=/home/superset/:$PYTHONPATH
+
 # check to see if the superset config already exists, if it does skip to
 # running the user supplied docker-entrypoint.sh, note that this means
 # that users can copy over a prewritten superset config and that will be used
@@ -31,7 +28,6 @@ if [ ! -f $SUPERSET_HOME/superset_config.py ]; then
   touch $SUPERSET_HOME/superset_config.py
  
   cat > $SUPERSET_HOME/superset_config.py <<EOF
-
 ROW_LIMIT = ${SUP_ROW_LIMIT}
 WEBSERVER_THREADS = 4
 SUPERSET_WEBSERVER_PORT = 8088
@@ -40,7 +36,6 @@ SECRET_KEY = '${SUP_SECRET_KEY}'
 SQLALCHEMY_DATABASE_URI = '${SUP_META_DB_URI}'
 CSRF_ENABLED = ${SUP_CSRF_ENABLED}
 HTTP_HEADERS = {}
- 
  
 # https://stackoverflow.com/questions/48966344/assign-anonymoususermixin-to-a-real-user
 AUTH_TYPE = 3
@@ -54,10 +49,6 @@ ADDITIONAL_MIDDLEWARE = [PredefinedUserMiddleware]
  
 EOF
 fi
-
-sed -i '/^SQLALCHEMY_DATABASE_URI/d' superset_config.py 
-echo "SQLALCHEMY_DATABASE_URI = '$SUP_META_DB_URI'" >> $DOMINO_WORKING_DIR/superset_config.py  
- 
  
 # set up Superset if we haven't already
 if [ ! -f $SUPERSET_HOME/.setup-complete ]; then
@@ -68,8 +59,8 @@ if [ ! -f $SUPERSET_HOME/.setup-complete ]; then
   echo "Initializing database"
   superset db upgrade
   
-#   echo "Loading examples"
-#   superset load_examples
+  echo "Loading examples"
+  superset load_examples
  
   echo "Creating default roles and permissions"
   superset init
@@ -83,24 +74,3 @@ fi
  
 echo "Starting up Superset"
 gunicorn superset:app
- 
-###########
-# # Create an admin user (you will be prompted to set username, first and last name before setting a password)
-# echo "fabmanager create-admin"
-# fabmanager create-admin --app superset --username admin --password superset --firstname Admin --lastname Superset --email superset+admin@example.com
- 
-# # Initialize the database
-# echo "superset db upgrade"
-# superset db upgrade
- 
-# # Load some data to play with
-# echo "superset load examples"
-# superset load_examples
- 
-# # Create default roles and permissions
-# echo "superset init"
-# superset init
- 
-# # To start a development web server on port 8088, use -p to bind to another port
-# echo "superset runserver"
-# superset runserver -d
